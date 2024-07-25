b9176b83cb9f0ba56443e9fe5dd70d3b083ce998
begae <gdtmdf@gmail.com>
2024-07-26 00:20:51 +0900

    add prometheus

--- node.go
+++ node.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
 	"time"
@@ -12,11 +13,14 @@ import (
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
-	peer "github.com/libp2p/go-libp2p/core/peer"
-	peerstore "github.com/libp2p/go-libp2p/core/peerstore"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
 	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
 	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
+	rcmgr "github.com/libp2p/go-libp2p/p2p/host/resource-manager"
 	ma "github.com/multiformats/go-multiaddr"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/testground/sdk-go/network"
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
@@ -67,9 +71,33 @@ func node(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 	runenv.RecordMessage("before netclient.MustConfigureNetwork")
 	netclient.MustConfigureNetwork(ctx, netConfig)
 
-	containerAddr, _ := netclient.GetDataNetworkIP()
-	myAddr, _ := ma.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/3000", containerAddr.To4().String()))
-	host, _ := libp2p.New(libp2p.ListenAddrs(myAddr))
+	http.Handle("/debug/metrics/prometheus", promhttp.Handler())
+	rcmgr.MustRegisterWith(prometheus.DefaultRegisterer)
+
+	str, err := rcmgr.NewStatsTraceReporter()
+	if err != nil {
+		panic(err)
+	}
+
+	rmgr, err := rcmgr.NewResourceManager(rcmgr.NewFixedLimiter(rcmgr.DefaultLimits.AutoScale()), rcmgr.WithTraceReporter(str))
+	if err != nil {
+		panic(err)
+	}
+
+	containerAddr, err := netclient.GetDataNetworkIP()
+	if err != nil {
+		panic(err)
+	}
+
+	myAddr, err := ma.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/3000", containerAddr.To4().String()))
+	if err != nil {
+		panic(err)
+	}
+
+	host, err := libp2p.New(libp2p.ListenAddrs(myAddr), libp2p.ResourceManager(rmgr))
+	if err != nil {
+		panic(err)
+	}
 	runenv.RecordMessage("created libp2p host")
 
 	dhtOpts := []dht.Option{
@@ -77,8 +105,12 @@ func node(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 		dht.BucketSize(20),
 	}
 
-	kad, _ := dht.New(ctx, host, dhtOpts...)
-	err := kad.Bootstrap(ctx)
+	kad, err := dht.New(ctx, host, dhtOpts...)
+	if err != nil {
+		panic(err)
+	}
+
+	err = kad.Bootstrap(ctx)
 	if err != nil {
 		panic(err)
 	}
@@ -97,14 +129,18 @@ func node(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 		}
 	} else {
 
-		bootInfo, _ := tgSubscribe(ctx, client, runenv)
+		bootInfo, err := tgSubscribe(ctx, client, runenv)
+		if err != nil {
+			panic(err)
+		}
+
 		bootstrap = bootInfo
 
 		runenv.RecordMessage("received bootstrap info, adding address...")
 		host.Peerstore().AddAddr(bootstrap.ID, bootstrap.Addrs[0], peerstore.PermanentAddrTTL)
 
 		runenv.RecordMessage("all peers ready, connecting...")
-		err := host.Connect(ctx, bootstrap)
+		err = host.Connect(ctx, bootstrap)
 		if err != nil {
 			fmt.Printf("Failed connecting to %s, error: %s\n", bootstrap.ID, err)
 		} else {
@@ -115,8 +151,15 @@ func node(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 	routingDiscovery := drouting.NewRoutingDiscovery(kad)
 	dutil.Advertise(ctx, routingDiscovery, *topicNameFlag)
 
-	gossipsub, _ := pubsub.NewGossipSub(ctx, host)
-	topic, _ := gossipsub.Join(*topicNameFlag)
+	gossipsub, err := pubsub.NewGossipSub(ctx, host)
+	if err != nil {
+		panic(err)
+	}
+
+	topic, err := gossipsub.Join(*topicNameFlag)
+	if err != nil {
+		panic(err)
+	}
 	runenv.RecordMessage("joined gossipsub topic")
 
 	if runenv.TestGroupID == "publisher" {
@@ -134,7 +177,7 @@ func node(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 }
 
 func streamFileTo(ctx context.Context, topic *pubsub.Topic, filePath string) {
-	time.Sleep(30 * time.Second)
+	time.Sleep(20 * time.Second)
 	data, err := os.ReadFile(filePath)
 	if err != nil {
 		fmt.Println("Error reading file:", err)
